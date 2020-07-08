c683a391c2fad58695bc0d463b6befed97b51357
Priya <pvaidya56@gmail.com>
2020-07-08 15:22:13 -0700

    completed lookup_table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,13 +1,16 @@
 # Your code here
+import math
+import random
 
 
-def slowfun_too_slow(x, y):
-    v = math.pow(x, y)
-    v = math.factorial(v)
-    v //= (x + y)
-    v %= 982451653
 
-    return v
+# def slowfun_too_slow(x, y):
+#     v = math.pow(x, y)
+#     v = math.factorial(v)
+#     v //= (x + y)
+#     v %= 982451653
+
+#     return v
 
 def slowfun(x, y):
     """
@@ -15,7 +18,17 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    return lookupTable.get(x, y)
 
+lookupTable = {}
+for x in range(2, 14):
+    for y in range(3, 6):
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+
+        lookupTable[(x, y)] = v
 
 
 # Do not modify below this line!
